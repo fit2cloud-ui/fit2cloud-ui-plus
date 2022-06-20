71a73eaad03a869601122c73b719a3e248d704a2
wangdan <dan.wang@fit2cloud.com>
2022-06-20 17:31:04 +0800

    fix(组件): table问题修复

--- src/components/table/FuTableBody.ts
+++ src/components/table/FuTableBody.ts
@@ -1,40 +1,46 @@
-import { h } from 'vue'
+import { h } from "vue";
 
 const isFix = (node: any) => {
-  const includeTag = node.type.name.indexOf("FuTableColumnDropdown") >= 0
-  const { fix } = node.props
-  let { type } = node.props
-  return (fix !== undefined && fix !== false) || ["selection", "index", "expand"].includes(type) || includeTag
-}
+  const includeTag = node.type.name.indexOf("FuTableColumnDropdown") >= 0;
+  const { fix } = node.props;
+  let { type } = node.props;
+  return (
+    (fix !== undefined && fix !== false) ||
+    ["selection", "index", "expand"].includes(type) ||
+    includeTag
+  );
+};
 
 const getLabel = (node: any) => {
-  if (node.props.label) return node.props.label
-  const prefix = "FU-T-"
-  const includeTag = node.type.name.indexOf("FuTableColumnDropdown") >= 0
-  let { label, type } = node.props
-  if (includeTag) label = prefix + "dropdown"
-  label ??= node.props.label
-  label ??= prefix + type
+  if (node.props.label) return node.props.label;
+  const prefix = "FU-T-";
+  const includeTag = node.type.name.indexOf("FuTableColumnDropdown") >= 0;
+  let { label, type } = node.props;
+  if (includeTag) label = prefix + "dropdown";
+  label ??= node.props.label;
+  label ??= prefix + type;
   return label;
-}
+};
+
 const FuTableBody = (props: any, context: any) => {
-  const slots = context.slots.default()
+  let slots = context.slots.default();
   const nodes: any = [];
-  let { columns } = props
-  const children = slots[0].children.filter((c: any) => c.type.name !== undefined)
-  if (!children) return nodes
-  if (!columns || columns?.length === 0) return children
+  let { columns } = props;
+  if (typeof slots[0].children[0].type === "symbol") {
+    slots = slots[0].children;
+  }
+  const children = slots[0].children.filter((c: any) => c.type.name !== undefined);
+  if (!children) return nodes;
+  if (!columns || columns?.length === 0) return children;
   columns.forEach((col: any) => {
     let node = children.find((child: any) => {
-      return col.label === getLabel(child)
-    })
+      return col.label === getLabel(child);
+    });
     if (node && (isFix(node) || col.show !== false)) {
       nodes.push(node);
     }
-  })
-  return nodes
-}
+  });
+  return nodes;
+};
 
 export default FuTableBody;
-
-
