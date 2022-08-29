0cf0d2e2e3d6cd6b30a384cf406307eedda74eba
mokun <kun@fit2cloud.com>
2022-08-29 10:13:35 +0800

    docs(文档): 更新文档

--- docs/assets/index.d996e784.js
+++ docs/assets/index.d996e784.js
@@ -1,4 +1,4 @@
-import{d as h,b as f,r as o,o as B,e as C,a as t,w as i,_ as F,f as w,t as y,g,c as E,l as k,F as D,j as b,u as O,k as q,p as G,i as H}from"./index.aff1f164.js";function A(u){u.__sourceCode=`<template>
+import{d as h,b as f,r as o,o as B,e as C,a as t,w as i,_ as F,f as w,t as y,g,c as E,l as k,F as D,j as b,u as O,k as q,p as G,i as H}from"./index.a69fdc4d.js";function A(u){u.__sourceCode=`<template>
   <div style="line-height: 40px;">
     <fu-read-write-switch :data="input" v-model="write" @change="change">
       <el-input v-model="input" @blur="blur" @keydown="keydown" />
