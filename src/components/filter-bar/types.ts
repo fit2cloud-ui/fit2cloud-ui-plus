b510d0e305385919c616cdbbe2478ef08211a1c5
mokun <kun@fit2cloud.com>
2023-01-05 18:25:44 +0800

    docs(文档): FuFilterOption增加boolean和object类型数据

--- src/components/filter-bar/types.ts
+++ src/components/filter-bar/types.ts
@@ -3,9 +3,9 @@ import {InjectionKey, Ref} from "vue";
 export const selectKey = Symbol('SelectKey') as unknown as InjectionKey<SelectContext>
 
 export interface SelectContext {
-  selection: Ref<Array<string | number> | string | number>
+  selection: Ref<Array<string | number | boolean | object> | string | number | boolean | object>
 
-  setSelected(value: string | number, selected: boolean): void
+  setSelected(value: string | number | boolean | object, selected: boolean): void
 }
 
 export const referenceKey = Symbol('ReferenceKey') as unknown as InjectionKey<Ref<ReferenceContext[]>>
