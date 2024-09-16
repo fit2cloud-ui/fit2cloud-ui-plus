d44b001d6e6ae82be5dd1b8b67e8a0b73f3691b1
Seied Ali Mirkarimi <84094431+Notorious-Ali@users.noreply.github.com>
2024-09-17 00:36:17 +0330

    add Persian fa-IR language
    Integrate Persian language support into Fit2Cloud UI Plus.

---
+++ src/locale/lang/fa.ts
@@ -0,0 +1,58 @@
+export default {
+  name: 'fa',
+  fu: {
+    filter_bar: {
+      filter: "فیلتر",
+      results: "موارد",
+      clear: "پاک کردن",
+      drawer_title: "فیلتر",
+      cancel: "لغو",
+      search: "جستجو",
+      select_all: "انتخاب همه",
+      more: "بیشتر"
+    },
+    search_bar: {
+      search: "جستجو",
+      adv_search: "جستجوی پیشرفته",
+      ok: "تایید",
+      cancel: "لغو",
+      please_select: "لطفا انتخاب کنید",
+      please_input: "لطفا وارد کنید",
+      like: "شامل",
+      not_like: "شامل نمی‌شود",
+      in: "متعلق به",
+      not_in: "متعلق نیست",
+      gt: "بزرگتر از",
+      ge: "بزرگتر یا مساوی",
+      lt: "کوچکتر از",
+      le: "کوچکتر یا مساوی",
+      eq: "برابر با",
+      ne: "نابرابر با",
+      between: "بین",
+      select_date: 'انتخاب تاریخ',
+      start_date: 'تاریخ شروع',
+      end_date: 'تاریخ پایان',
+      select_date_time: 'انتخاب تاریخ و زمان',
+      start_date_time: 'تاریخ و زمان شروع',
+      end_date_time: 'تاریخ و زمان پایان',
+      range_separator: "تا",
+      data_time_error: "تاریخ شروع نمی‌تواند بزرگتر از تاریخ پایان باشد",
+      clean: "پاک کردن",
+      refresh: "بازخوانی"      
+    },
+    table: {
+      ok: "تایید",
+      reset: "بازنشانی",
+      custom_table_fields: "فیلدهای جدول سفارشی",
+      custom_table_fields_desc: "فیلد ثابت غیرقابل انتخاب است، سایر موارد قابل بررسی و کشیدن هستند",
+      custom_table_rows: "ردیف‌های جدول",
+      more: "بیشتر"      
+    },
+    steps: {
+      cancel: "لغو",
+      next: "بعدی",
+      prev: "قبلی",
+      finish: "تایید",
+    }
+  }
+}
