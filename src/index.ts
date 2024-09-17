52762888ad2bc4fc15d4af8da9c4521f7356b292
Seied Ali Mirkarimi <84094431+Notorious-Ali@users.noreply.github.com>
2024-09-17 22:56:00 +0330

    Resolving 'globEager is not a function' Error in Component Import Process
    index-B0xWKXmx.js:5 Uncaught TypeError: (intermediate value).globEager is not a function
        at index-B0xWKXmx.js:5:83765
     `var Uu = L4(function(e) {
        return _S(ui(e, 1, ig, !0))
    });
    const Mh = import.meta.globEager("@/components/*/index.ts")
      , wS = (e, t) => {
        Object.keys(Mh).forEach(n => {
            let o = Mh[n].default;
            e.use(o)
        }
        ),
        Yw(t)`

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,24 @@ import PackageJSON from "../package.json"
 import type {App} from 'vue'
 import {GlobalConfig, provideGlobalConfig} from "@/hooks";
 
-const components = import.meta.globEager('@/components/*/index.ts');
+const components = [FilterBar, IconButton, ReadWriteSwitch,SearchBar,SpeedDial,Steps, table,tabs/* ... other components */];
+import FilterBar from '@/components/filter-bar/index';
+import IconButton from '@/components/icon-button/index';
+import ReadWriteSwitch from '@/components/read-write-switch/index';
+import SearchBar from '@/components/search-bar/index';
+import SpeedDial from '@/components/speed-dial/index';
+import Steps from '@/components/steps/index';
+import table from '@/components/table/index';
+import tabs from '@/components/tabs/index';
 
 const install = (app: App, config: GlobalConfig): void => {
-  Object.keys(components).forEach(key => {
-    let component = components[key].default;
-    app.use(component)
-  })
-  provideGlobalConfig(config)
+  components.forEach(component => {
+    if (typeof component === 'object' && 'install' in component) {
+      app.use(component);
+    } else if (typeof component === 'function' && component.name) {
+      app.component(component.name, component);
+    }
+  });  provideGlobalConfig(config)
 }
 
 const plugin = {
