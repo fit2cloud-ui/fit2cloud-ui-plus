82146a4fc4c24d3bfa9a2fde737f2374222507ac
q4speed <kun@fit2cloud.com>
2024-12-06 08:22:10 +0000

    github action update docs

--- docs/assets/index.5476ccd5.js
+++ docs/assets/index.5476ccd5.js
@@ -1,4 +1,4 @@
-import{d as g,r,a as u,o as B,c as E,f as n,b as e,w as d,t as C,F as D,_ as x,p as T,h as I,g as H,n as J,e as K,i as h,u as Q,j as W}from"./index.61be0b2a.js";function k(l){l.__sourceCode=`<template>
+import{d as g,r,a as u,o as B,c as E,f as n,b as e,w as d,t as C,F as D,_ as x,p as T,h as I,g as H,n as J,e as K,i as h,u as Q,j as W}from"./index.694a7dd1.js";function k(l){l.__sourceCode=`<template>
   <div>
     <h4>
       1\u3001\u4F7F\u7528slot
